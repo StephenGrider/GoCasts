f3c5e4789e7c823d01d75868564d8733cec45f7e
stephengrider <ste.grider@gmail.com>
2017-08-07 16:35:31 -0700

    slice range

--- code/cards/deck.go
+++ code/cards/deck.go
@@ -6,6 +6,21 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
--- code/cards/main.go
+++ code/cards/main.go
@@ -1,12 +1,7 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
