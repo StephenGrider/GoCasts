c2836ccf296006a73b09ee7f34154d9001c33450
stephengrider <ste.grider@gmail.com>
2017-08-07 13:00:42 -0700

    functions and return types

--- code/cards/main.go
+++ code/cards/main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	card = "Five of Diamonds"
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
