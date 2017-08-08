de6778e842cc3ea3b437d3b04373fb2f215e0ddf
stephengrider <ste.grider@gmail.com>
2017-08-08 15:06:50 -0700

    shuffle func

--- code/cards/deck.go
+++ code/cards/deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
 )
@@ -55,4 +56,12 @@ func newDeckFromFile(filename string) deck {
 
 	s := strings.Split(string(bs), ",")
 	return deck(s)
+}
+
+func (d deck) shuffle() {
+	for i := range d {
+		newPosition := rand.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- code/cards/main.go
+++ code/cards/main.go
@@ -1,6 +1,7 @@
 package main
 
 func main() {
-	cards := newDeckFromFile("my")
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
 }
