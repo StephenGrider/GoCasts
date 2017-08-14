8927b742060e7758d1b03366d55031534cf32892
stephengrider <ste.grider@gmail.com>
2017-08-14 14:52:55 -0700

    added interface

--- code/interfaces/main.go
+++ code/interfaces/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type bot interface {
+	getGreeting() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
@@ -10,16 +14,12 @@ func main() {
 	sb := spanishBot{}
 
 	printGreeting(eb)
-	// printGreeting(sb)
+	printGreeting(sb)
 }
 
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
-
-// func printGreeting(sb spanishBot) {
-// 	fmt.Println(sb.getGreeting())
-// }
 
 func (englishBot) getGreeting() string {
 	// VERY custom logic for generating an english greeting
