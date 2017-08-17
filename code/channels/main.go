9f454bb8c30cdf09f7e0848753d6472a4d6be4c4
stephengrider <ste.grider@gmail.com>
2017-08-17 11:49:38 -0700

    status checker

---
+++ code/channels/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	for _, link := range links {
+
+	}
+}
