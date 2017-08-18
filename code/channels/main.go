33c686dfd865def4c1c3e92cf82d7f5e19112e3f
stephengrider <ste.grider@gmail.com>
2017-08-18 15:36:25 -0700

    func literals

--- code/channels/main.go
+++ code/channels/main.go
@@ -22,12 +22,15 @@ func main() {
 	}
 
 	for l := range c {
-		time.Sleep(5 * time.Second)
-		go checkLink(l, c)
+		go func() {
+			time.Sleep(5 * time.Second)
+			checkLink(l, c)
+		}()
 	}
 }
 
 func checkLink(link string, c chan string) {
+	time.Sleep(5 * time.Second)
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
